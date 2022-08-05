13dba4dc22f5bacbc71ec4b93109824a479b3653
TokcDK <TokcDK@Gmail.com>
2022-08-05 14:34:21 +0500

    added extra strings in skip list

--- DeadBodyCleanupNPCAttacher/Settings.cs
+++ DeadBodyCleanupNPCAttacher/Settings.cs
@@ -15,10 +15,13 @@ namespace DeadBodyCleanupNPCAttacher
         [SynthesisTooltip("List of string keywords to skip npc by their editor id")]
         public HashSet<StringCompareSettingContainer> SkipList = new()
         {
+            new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="audiotemplate" } },
+            new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="voicetype" } },
             new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="corpse", Comment="dont touch corpse npc" } },
             new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="dummy", Comment="some dummy npc" } },
             new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="preset", Comment="some preset npc" } },
             new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="cb2_", Compare=CompareType.StartsWith, Comment="dummy npc from 'Collector bags 2' mod" } },
+            new StringCompareSettingContainer(){ StringSetting=new StringCompareSetting(){ Name="fstest", Compare=CompareType.StartsWith, Comment="some test npc" } },
         };
     }
 }
